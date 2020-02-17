5e1d82c927fef7e09b0b6ea985013b7518927d83
windfly <jifyzu@163.com>
2020-02-17 16:03:58 +0800

    websocket

--- src/component/GateWaySocket.js
+++ src/component/GateWaySocket.js
@@ -2,45 +2,67 @@ import React, {Component} from 'react';
 import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 
+
 export default class GateWaySocket extends Component {
     constructor() {
         super();
-        this.state = {message: ""};
-        this.stompClient=null;
+        this.state = {message: []};
+        this.stompClient = null;
+        this.messages = [];
     }
 
     connection = () => {
         var socket = new SockJS("http://localhost:9010/websocket");
-
         this.stompClient = Stomp.over(socket)
         console.log(this.stompClient)
-        this.stompClient.connect({}, function (frame) {
-            console.log("connected: " + frame);
-            this.stompClient.subscribe('/topic', function (response) {
-                this.setState({message: response.body})
-            })
+        var fromUser = this.fromUser.value;
+        var thisComponent = this;
+        this.stompClient.connect({}, function (frame) {//连接WebSocket服务端
+            console.log('Connected:' + frame);
+            thisComponent.stompClient.subscribe("/user/" + fromUser + "/message", function (response) {
+                var data = JSON.parse(response.body);
+                console.log(data.content);
+                thisComponent.messages.push(data.fromUser + ":" + data.content);
+                thisComponent.setState({message: thisComponent.messages})
+            });
         });
     }
 
     sendMessage = () => {
-        var message = "{'to':'" + this.to.value + "','message':'" + this.message.value + "'}";
-        this.stompClient.send("/chat", {}, message);
+        var fromUser = this.fromUser.value
+        var toUser = this.toUser.value
+        var content = this.content.value
+        this.stompClient.send("/message", {}, JSON.stringify({
+            fromUser: fromUser,
+            toUser: toUser,
+            content: content
+        }));
+        this.messages.push(fromUser + ":" + content);
+        this.content.value = "";
+        this.setState({message: this.messages})
+    }
 
+    disconnect = () => {
+        if (this.stompClient != null) {
+            this.stompClient.disconnect();
+        }
     }
 
 
     render() {
-        console.log(this.props)
+
         return (
             <div>
-                <div><input type="text" ref={val => this.sid = val}/>
+                <div>发送人:<input type="text" ref={val => this.fromUser = val}/></div>
+                <div>
                     <button onClick={this.connection}>连接</button>
                 </div>
-                <div>内容接受者:<input type="text" ref={val => this.to = val}/></div>
-                <div>发送内容:<input type="text" ref={val => this.message = val}/>
+                <div>接收人:<input type="text" ref={val => this.toUser = val}/></div>
+                <div>发送内容:<input type="text" ref={val => this.content = val}/>
                     <button onClick={this.sendMessage}>发送</button>
                 </div>
-                <div>{this.state.message}</div>
+                <div>{this.state.message.map((item, index) => (<p key={index}>{item}</p>)
+                )}</div>
 
             </div>
         )
