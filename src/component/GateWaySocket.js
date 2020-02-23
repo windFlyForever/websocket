89401f2494af2005d71af8b3ad25e7860da3cf40
windfly <jifyzu@163.com>
2020-02-23 21:52:39 +0800

    sockjs

--- src/component/GateWaySocket.js
+++ src/component/GateWaySocket.js
@@ -6,15 +6,20 @@ import Stomp from 'stompjs';
 export default class GateWaySocket extends Component {
     constructor() {
         super();
-        this.state = {message: []};
+        this.state = {
+            message: [],
+            connectInfo: ""
+        };
         this.stompClient = null;
         this.messages = [];
     }
 
     connection = () => {
-        var socket = new SockJS("http://localhost:9010/websocket");
+        //gateway地址+websocket服务地址http://localhost:9000/websocket+/websocket
+        var socket = new SockJS("http://localhost:9000/websocket/websocket");
         this.stompClient = Stomp.over(socket)
         console.log(this.stompClient)
+
         var fromUser = this.fromUser.value;
         var thisComponent = this;
         this.stompClient.connect({}, function (frame) {//连接WebSocket服务端
@@ -26,9 +31,15 @@ export default class GateWaySocket extends Component {
                 thisComponent.setState({message: thisComponent.messages})
             });
         });
+
     }
 
     sendMessage = () => {
+        if (Object.keys(this.stompClient).length === 0 || !this.stompClient.connected) {
+            this.setState({connectInfo: "socket未连接成功，请重新连接"});
+            return;
+        }
+
         var fromUser = this.fromUser.value
         var toUser = this.toUser.value
         var content = this.content.value
@@ -43,8 +54,10 @@ export default class GateWaySocket extends Component {
     }
 
     disconnect = () => {
-        if (this.stompClient != null) {
+        if (Object.keys(this.stompClient).length !== 0 && this.stompClient.connected) {
             this.stompClient.disconnect();
+            console.log(this.stompClient)
+            this.setState({connectInfo: "连接断开"})
         }
     }
 
@@ -55,8 +68,14 @@ export default class GateWaySocket extends Component {
             <div>
                 <div>发送人:<input type="text" ref={val => this.fromUser = val}/></div>
                 <div>
-                    <button onClick={this.connection}>连接</button>
+                    <div>
+                        <button onClick={this.connection}>连接</button>
+                        <button onClick={this.disconnect}>断开连接</button>
+                    </div>
+
+                    <div>{this.state.connectInfo}</div>
                 </div>
+
                 <div>接收人:<input type="text" ref={val => this.toUser = val}/></div>
                 <div>发送内容:<input type="text" ref={val => this.content = val}/>
                     <button onClick={this.sendMessage}>发送</button>
